8bc5b1cef6685938891659995304d507e5a813f9
iainh <cenaia.llc@gmail.com>
2026-02-24 04:27:57 +0000

    config: add static export for demo deployment

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/eywa',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+};
 module.exports = nextConfig;
